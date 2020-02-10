bfc8dccb58f5981c15493eaae34bed929b697816
Jonas I. Liechti <j-i-l@users.noreply.github.com>
2020-02-10 01:01:27 +0100

    Update __init__.py

--- majortrack/__init__.py
+++ majortrack/__init__.py
@@ -1,4 +1,9 @@
+r"""
+This python package implements the evolutionary clustering algorithm
+presented in the paper by :cite:`liechti2019time`.
+
+.. bibliography:: ../references.bib
 """
-"""
+
 
 from .tracker import MajorTrack
